04f5c43ebe9cf1b616dd7725a962b59b1f0b928b
masatomo-sugawara <masatomo.s1982@gmail.com>
2022-07-14 07:57:59 +0900

    おまけ(useMemo)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -12,6 +12,9 @@ export default function App() {
   const onClickOpen = () => {
     setOpen(!open);
   };
+  const onClickClose = useCallback(() => {
+    setOpen(false);
+  }, []);
 
   return (
     <div className="App">
@@ -19,7 +22,7 @@ export default function App() {
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
       {/* <p>{text}</p> */}
     </div>
   );
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,24 +1,27 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "khaki"
 };
 
-export const ChildArea = (props) => {
+export const ChildArea = memo((props) => {
   const data = [...Array(1000).keys()];
   console.log("ChildAreaがレンダリングされた！！");
   console.log(data);
   data.forEach(() => {
     console.log("...");
   });
-  const { open } = props;
+  const { open, onClickClose } = props;
   return (
     <>
       {open ? (
         <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
   );
-};
+});
